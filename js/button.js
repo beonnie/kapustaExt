8e60aeaf094189b0f3f4e41218b8d0bc95e5bec9
beonnie <denis.shimonov@mail.ru>
2024-03-19 16:14:17 +0300

    add icons and base page

---
+++ js/button.js
@@ -0,0 +1,10 @@
+const renderButton = document.getElementsByClassName("renderButton")[0];
+const dealButton = document.getElementsByClassName("dealButton")[0];
+const borrowButton = document.getElementsByClassName("borrowButton")[0];
+const lendButton = document.getElementsByClassName("lendButton")[0];
+
+if (borrowButton) {
+    borrowButton.addEventListener("click", (e) => {
+
+    });
+  }
