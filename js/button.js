66ecd07f03d01f73f48903c3ae2ccea0cde3454a
beonnie <denis.shimonov@mail.ru>
2024-03-19 16:54:22 +0300

    f

--- js/button.js
+++ js/button.js
@@ -3,8 +3,11 @@ const dealButton = document.getElementsByClassName("dealButton")[0];
 const borrowButton = document.getElementsByClassName("borrowButton")[0];
 const lendButton = document.getElementsByClassName("lendButton")[0];
 
-if (borrowButton) {
-    borrowButton.addEventListener("click", (e) => {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.borrowButton').addEventListener('click', clickHandler);
+    main();
+  });
 
-    });
-  }
+function  clickHandler(){
+    window.location.replace("fff")
+}
