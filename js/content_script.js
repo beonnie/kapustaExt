ad3a823e88c11b6aec45b20389c694f68507cab9
beonnie <denis.shimonov@mail.ru>
2024-03-21 15:33:36 +0300

    a

--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,14 @@
-chrome.runtime.onMessage.addListener(
-   () => {
-      alert("fsdfsd");
-   }
- )
+const a = function(){
+   const b = document.querySelectorAll('div');
+   console.log(b.length);
+   
+   const script = document.createElement('script');
+   script.innerText = 'const a = document.querySelectorAll(".more-offers-block-btn-block");a[1].firstChild.click();a[1].firstChild.click();a[1].firstChild.click();a[1].firstChild.click();';
+   b[40].append(script);
+  
+
+   
+}
+
+a();
+
